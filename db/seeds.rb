0ee79f879e98bfcf8000e382efd3ce10f0e0934d
Tomomi <madeinwatashi@gmail.com>
2017-09-15 15:28:51 +1000

    Add devise gem, model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# tweets = [
+#     {message: 'I like cake'},
+#     {message: 'I like cats'},
+#     {message: 'I like Jake\'s cake'}
+# ]
+
+# Tweet.create(tweets)
+
+puts 'done'
